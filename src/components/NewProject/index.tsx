cf0f8bd9c704cb48c54b3ae74e0c3df5f484e438
Igor <igorgg@gmail.com>
2023-02-08 20:11:34 -0300

    hotfix video width

--- src/components/NewProject/index.tsx
+++ src/components/NewProject/index.tsx
@@ -2,20 +2,22 @@ import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const NewProject = () => {
   return (
-    <Container maxW={"5xl"}>
+    <Container maxW={"7xl"}>
       <Flex flexDir={{ base: "column", md: "row" }} gap="2rem">
-        <video
-          autoPlay
-          loop
-          playsInline
-          preload="auto"
-          muted
-          width="500px"
-          src="https://video.wixstatic.com/video/e306a5_ac3e3a978c5a489584f7f75ccc9eacaa/480p/mp4/file.mp4"
-        />
-        <Flex flexDir={"column"} gap="1rem">
+        <Box w={{ base: "100%", md: "120%" }}>
+          <video
+            autoPlay
+            loop
+            playsInline
+            preload="auto"
+            muted
+            width="100%"
+            src="https://video.wixstatic.com/video/e306a5_ac3e3a978c5a489584f7f75ccc9eacaa/480p/mp4/file.mp4"
+          />
+        </Box>
+        <Flex flexDir={"column"} gap="1rem" w="100%">
           <Heading size={"lg"}>Residencial Outeiro do Sul</Heading>
-          <Text>
+          <Text mt="2rem">
             Localizado no alto da zona sul da cidade de Ribeirão Preto/SP, o
             Edifício Outeiro do Sul oferece 82,77 metros quadrados de muito
             conforto e sofisticação. Cada apartamento possui 2 suítes, varanda
